42e191b166316aeafc0431c842ede971346b7ea7
Hadi Hosseinpour <hadi1998goodboy@gmail.com>
2022-01-03 10:44:43 +0330

    edit

---
+++ components/constants.js
@@ -0,0 +1,7 @@
+export const apiURL = 'https://academy.honari.com';
+export const loginURL = 'http://honari.com/user';
+export const authURL = 'http://auth.honari.com';
+export const classNextURL = 'https://honari.com/academy';
+export const shopURL = 'https://honari.com';
+export const imageURL = 'https://academy.honari.com/warehouse/images';
+export const cookieURL = 'honari.com';
